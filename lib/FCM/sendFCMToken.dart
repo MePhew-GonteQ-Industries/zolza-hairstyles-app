7ae7826d6e429fb01cd2b5f84ad7a89e3270f455
biQte <patrykgonet@gmail.com>
2022-06-11 09:51:27 +0200

    IDK

--- lib/FCM/sendFCMToken.dart
+++ lib/FCM/sendFCMToken.dart
@@ -4,11 +4,11 @@ import 'package:http/http.dart' as http;
 Future<http.Response> sendFCMToken(String token) {
   return http.post(
     Uri.parse(TemporaryStorage.apiUrl),
-    // Uri.parse(TemporaryStorage.apiUrl + '/notifications/fcmtoken'),
-    // // headers: {
-    // //   'Content-Type': 'application/json',
-    // //   'Authorization': 'Bearer ${TemporaryStorage.accessToken}',
-    // // },
+    // Uri.parse(TemporaryStorage.apiUrl + '/notifications/add_token'),
+    // headers: {
+    //   'Content-Type': 'application/json',
+    //   'Authorization': 'Bearer ${TemporaryStorage.accessToken}',
+    // },
     // body: {
     //   'fcmtoken': token,
     // },
