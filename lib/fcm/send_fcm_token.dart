16b1a56f0db529c85c9beb3f425472a6d8491df6
biQte <patrykgonet@gmail.com>
2022-06-12 14:03:11 +0200

    Added fcm_token sending to database

--- lib/fcm/send_fcm_token.dart
+++ lib/fcm/send_fcm_token.dart
@@ -1,16 +1,19 @@
+import 'dart:convert';
+
 import 'package:hairdressing_salon_app/constants/globals.dart';
 import 'package:http/http.dart' as http;
+import '../helpers/user_data.dart';
 
 Future<http.Response> sendFCMToken(String token) {
   return http.post(
-    Uri.parse(apiUrl),
-    // Uri.parse($apiUrl/notifications/add_token),
-    // headers: {
-    //   'Content-Type': 'application/json',
-    //   'Authorization': 'Bearer ${UserData.accessToken}',
-    // },
-    // body: {
-    //   'fcm_token': token,
-    // },
+    // Uri.parse(apiUrl),
+    Uri.parse('$apiUrl/notifications/add_token'),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ${UserData.accessToken}',
+    },
+    body: jsonEncode({
+      'fcm_token': token,
+    }),
   );
 }
